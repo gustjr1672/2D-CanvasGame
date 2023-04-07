092bc57bf01ad3ca08e62fb2672e333cf1aad069
HyeonSeok <gustjr2332@naver.com>
2023-04-07 20:29:42 +0900

    feat: Add character object in gameCanvas #5

--- characterSelect/ui/gameCanvas.js
+++ characterSelect/ui/gameCanvas.js
@@ -1,5 +1,7 @@
 import Background from "../item/background.js";
 import MaskDude from "../item/characters/maskDude.js";
+import PinkMan from "../item/characters/pinkMan.js";
+import VirtualGuy from "../item/characters/virtualGuy.js";
 
 export default class gameCanvas {
   #canvas;
@@ -17,12 +19,20 @@ export default class gameCanvas {
     this.#ctx = this.#canvas.getContext("2d");
 
     this.#background = new Background();
-    this.#characters = [new MaskDude({ x: 340, y: 380 })];
+
+    this.#characters = [
+      new MaskDude({ x: 1140, y: 380 }),
+      new PinkMan({ x: 740, y: 380 }),
+      new VirtualGuy({ x: 340, y: 380 }),
+    ];
   }
 
   paint() {
     this.#background.draw(this.#ctx);
-    this.#characters[0].draw(this.#ctx);
+
+    for (let character of this.#characters) {
+      character.draw(this.#ctx);
+    }
   }
 
   run() {
