c80035f6244e6aef874296c16ac2f455efe388dc
wognr <wognr123@gmail.com>
2023-04-07 14:59:21 +0900

    feat: Draw selectLevel #2

--- stageSelect/ui/gameCanvas.js
+++ stageSelect/ui/gameCanvas.js
@@ -1,9 +1,11 @@
 import Background from "../item/background.js";
+import SelectLevel from "../item/selectLevel.js";
 
 export default class GameCanvas {
   #canvas;
   #ctx;
   #background;
+  #selectLevel;
   constructor() {
     this.#canvas = document.createElement("canvas");
     document.body.append(this.#canvas);
@@ -11,10 +13,12 @@ export default class GameCanvas {
     this.#canvas.width = 1520;
     this.#canvas.height = 850;
     this.#background = new Background();
+    this.#selectLevel = new SelectLevel();
   }
 
   paint() {
     this.#background.draw(this.#ctx);
+    this.#selectLevel.draw(this.#ctx);
   }
 
   run() {
