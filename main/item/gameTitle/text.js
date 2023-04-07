28cdd2e6dd2e1aae83afc1a500ab3bbd6481b69f
HyeonSeok <gustjr2332@naver.com>
2023-04-07 16:56:38 +0900

    feat: Add gametitle text image draw in parents class #1

---
+++ main/item/gameTitle/text.js
@@ -0,0 +1,26 @@
+export default class Text {
+  #position;
+  #width;
+  #height;
+  #positionToMove;
+  #img;
+  #velocityX;
+
+  constructor({ x, y }, width, height, positionToMove, velocityX, img) {
+    this.#position = {
+      x,
+      y,
+    };
+
+    this.#width = width;
+    this.#height = height;
+    this.#positionToMove = positionToMove;
+    this.#velocityX = velocityX;
+
+    this.#img = img;
+  }
+
+  draw(ctx) {
+    ctx.drawImage(this.#img, this.#position.x, this.#position.y, this.#width, this.#height);
+  }
+}
